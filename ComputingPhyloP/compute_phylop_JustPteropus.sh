85b733c309173633b2ae633afd9f5d68ff0e111a
astarr97 <77601457+astarr97@users.noreply.github.com>
2025-03-20 12:10:52 -0700

    Add files via upload

---
+++ ComputingPhyloP/compute_phylop_JustPteropus.sh
@@ -0,0 +1,18 @@
+to_mask="Pteropus_alecto"
+
+phylop_species="Pteropus_alecto"
+folder_name="Pteropus_alecto_PhyloP_MaskPteAle"
+#cp -r /home/groups/hbfraser/Common_Software /scratch/users/astarr97/Common_Software
+#mkdir /scratch/users/astarr97/astarr_scripts
+#cp -r /home/groups/hbfraser/astarr_scripts/AccelConvDist /scratch/users/astarr97/astarr_scripts/AccelConvDist
+
+/scratch/users/astarr97/Common_Software/cactus-bin-v2.6.13/bin/hal2fasta /scratch/users/astarr97/PhyloP/hg38.447way.hal $phylop_species > $phylop_species.fasta
+samtools faidx $phylop_species.fasta
+awk 'BEGIN {FS="\t"}; {print $1 FS "0" FS $2}' $phylop_species.fasta.fai | sort -n -r -k 3,3 > ${phylop_species}_contigs.bed
+
+python make_scripts_mask_phylop.py ${phylop_species}_contigs.bed $to_mask $phylop_species $folder_name
+
+cp /home/groups/hbfraser/astarr_scripts/AccelConv/driver.sh /scratch/users/astarr97/PhyloP/Feasibility/$folder_name
+cd $folder_name
+./driver.sh
+cd ..
